0982b448bf362e1279b6750986a144326f5c02db
v-sekulic <v.sekulic@vegait.rs>
2022-05-08 19:39:28 +0200

    Adds missing contenttypes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("https://test-strapi-deploy-heroku.herokuapp.com/"),
+  url: env("https://entasis-me-cms.herokuapp.com/"),
   proxy: true,
   app: {
     keys: env.array("APP_KEYS", ["kSQAai/wyArPK9EahB8O2w==", "+f1BxVLlEAKuVdnnrHrtnw=="]),
