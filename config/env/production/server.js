2093117f4098f786de6259c750a1674a7c4bdf18
v-sekulic <v.sekulic@vegait.rs>
2022-05-08 16:01:45 +0200

    api keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("https://test-strapi-deploy-heroku.herokuapp.com/"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", ["kSQAai/wyArPK9EahB8O2w==", "+f1BxVLlEAKuVdnnrHrtnw=="]),
   },
 });
